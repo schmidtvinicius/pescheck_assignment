7b7f1445a8be0b2aab6ff33417e27c9780e1a2aa
vinicius <vinimachado.s@gmail.com>
2020-09-13 00:30:05 +0200

    looking for crypto instead of cryptocurrency

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -7,7 +7,7 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = (f'http://newsapi.org/v2/everything?q=cryptocurrency&sortBy=publishedAt&apiKey={api_key}')
+    url = (f'http://newsapi.org/v2/everything?q=crypto&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
     json_object = response.json()
     articles_list = []
