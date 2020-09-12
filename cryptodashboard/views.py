71c9b5031a07acd97bc19d62d71fbec2ff3600f0
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:37:30 +0200

    printing retrieved data

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -10,4 +10,4 @@ def home(request):
     url = ('http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5')
     response = requests.get(url)
     print(response.json())
-    return HttpResponse('<p>testando</p>')
+    return HttpResponse(f'<p>{response.json()}</p>')
