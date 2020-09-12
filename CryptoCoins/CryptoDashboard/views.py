a75ff44a8514c5eba7e03bf7b6dff0b449adfd1e
vinicius <vinimachado.s@gmail.com>
2020-09-12 17:25:42 +0200

    Testing

--- CryptoCoins/CryptoDashboard/views.py
+++ CryptoCoins/CryptoDashboard/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-import requests
 
 # Create your views here.
 def home(request):
-    url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
-    result = requests.get(url)
-    content = result.read()
-    return content   
+    # url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
+    # result = requests.get(url)
+    # content = result.read()
+    # return content   
