54a210b338ed3d9c114b5daa3edbe0feaeccf0bb
vinicius <vinimachado.s@gmail.com>
2020-09-12 17:29:28 +0200

    Trying to make it work

--- CryptoCoins/CryptoDashboard/views.py
+++ CryptoCoins/CryptoDashboard/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    # url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
-    # result = requests.get(url)
-    # content = result.read()
-    # return content   
+    return 'Hello'
