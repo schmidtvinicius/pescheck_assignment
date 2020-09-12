b6597dc98e053350d4e7e952b57eee366ae2747a
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:22:08 +0200

    trying to show aqcuired data

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>Acessa la meu siteeeeeeeeee</p>')
+    url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
+    content = requests.get(url)
+    return HttpResponse(f'<p>{content}</p>')
