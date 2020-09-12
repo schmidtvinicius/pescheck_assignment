f353ea17ea325e325d322800921a52751e33d69d
vinicius <vinimachado.s@gmail.com>
2020-09-12 22:27:01 +0200

    test

--- CryptoCoins/CryptoCoins/settings.py
+++ CryptoCoins/CryptoCoins/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_on_heroku
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +122,4 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# django_on_heroku.settings(locals())
+django_on_heroku.settings(locals())
