5e094ac48b92771839c2d4cabc6bbffba37336e4
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:56:06 +0200

    check length json array

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -9,4 +9,4 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 def home(request):
     url = (f'http://newsapi.org/v2/everything?q=crypto-coins&from=2020-08-12&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
-    return HttpResponse(f'<p>{response.json()}</p>')
+    return HttpResponse(f'<p>{len(response.json())}</p>')
