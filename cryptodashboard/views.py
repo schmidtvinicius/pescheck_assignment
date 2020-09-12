87d84409ac8375f3df4c21462e998a788e264346
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:17:32 +0200

    yea boi

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<p>Hello world</p>')
+    return HttpResponse('<p>Acessa la meu siteeeeeeeeee</p>')
