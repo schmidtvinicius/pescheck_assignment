2b09691574a8fb417b0de45ee16462b7c89787b3
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:50:21 +0200

    trying to get info on other coins

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -3,11 +3,10 @@ from django.http import HttpResponse
 from newsapi import NewsApiClient
 import requests
 
-newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
+api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = ('http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5')
+    url = (f'http://newsapi.org/v2/everything?q=crypto-coins&from=2020-08-12&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
-    print(response.json())
     return HttpResponse(f'<p>{response.json()}</p>')
