715c0c8bd348731b32bb9dadfd49af4f8f5e2ab2
vinicius <vinimachado.s@gmail.com>
2020-09-13 00:24:21 +0200

    showing a shorter version of each article

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -7,7 +7,16 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = (f'http://newsapi.org/v2/everything?q=crypto&sortBy=publishedAt&apiKey={api_key}')
+    url = (f'http://newsapi.org/v2/everything?q=cryptocurrency&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
     json_object = response.json()
-    return HttpResponse(f'<p>{json_object["articles"]}</p>')
+    articles_list = []
+    for article in json_object["articles"]:
+        short_article = {
+            "source": article["source"],
+            "author": article["author"],
+            "title": article["title"],
+            "url": article["url"]
+        }
+        articles_list.append(short_article)
+    return HttpResponse(f'<p>{articles_list}</p>')
