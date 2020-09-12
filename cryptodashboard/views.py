0b36d805bd7627f095d16b4d649e85cae9f4013a
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:59:18 +0200

    remove published date

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -7,6 +7,6 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = (f'http://newsapi.org/v2/everything?q=crypto&from=2020-08-12&sortBy=publishedAt&apiKey={api_key}')
+    url = (f'http://newsapi.org/v2/everything?q=crypto&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
     return HttpResponse(f'<p>{len(response.json())}</p>')
