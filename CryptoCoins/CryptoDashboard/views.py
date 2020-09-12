45f24fd574d9af8e16f69125a621e16ec0236ae2
vinicius <vinimachado.s@gmail.com>
2020-09-12 17:05:20 +0200

    content gets returned to client

--- CryptoCoins/CryptoDashboard/views.py
+++ CryptoCoins/CryptoDashboard/views.py
@@ -6,4 +6,4 @@ def home(request):
     url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
     result = requests.get(url)
     content = result.read()
-    print(content)    
+    return content   
