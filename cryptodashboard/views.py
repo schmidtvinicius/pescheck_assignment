896acc1b481acf4eedfe1e4e7d1695d1275a22ac
vinicius <vinimachado.s@gmail.com>
2020-09-12 23:29:54 +0200

    trying to get the data

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from newsapi import NewsApiClient
 import requests
+
+newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 
 # Create your views here.
 def home(request):
-    url = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5'
-    content = requests.get(url)
-    return HttpResponse(f'<p>{content}</p>')
+    url = ('http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-12&sortBy=publishedAt&apiKey=3779ffddd95448f6ac0bc70bb87524e5')
+    response = requests.get(url)
+    print(response.json())
+    return HttpResponse('<p>testando</p>')
