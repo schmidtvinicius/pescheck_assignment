6dcb0f66b2e40a0dd6f5c76b7236ea6d60749989
vinicius <vinimachado.s@gmail.com>
2020-09-13 15:47:26 +0200

    imported load_crypto data sucessfully

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -4,14 +4,14 @@ from newsapi import NewsApiClient
 import requests
 import json
 from .models import CryptoCurrency
-import management.commands.load_crypto_data
+from .management.commands import load_crypto_data
 
 newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 
 # Create your views here.
 def home(request):
     if not CryptoCurrency.objects.exists():
-        management.commands.load_crypto_data
+        load_crypto_data
     return render(request, 'home.html', {
         'all_currencies': CryptoCurrency.objects.all(),
     })
