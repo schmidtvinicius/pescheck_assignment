e3ee3c24623ac14b3119ac26161f2df9c47ac5c8
vinicius <vinimachado.s@gmail.com>
2020-09-13 11:39:58 +0200

    rendering a html page

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -8,17 +8,18 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 # Create your views here.
 def home(request):
     url = (f'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey={api_key}')
-    response = requests.get(url)
-    json_object = response.json()
-    total_results = json_object["totalResults"]
-    articles_list = []
-    for article in json_object["articles"]:
-        short_article = {
-            "source": article["source"],
-            "author": article["author"],
-            "title": article["title"],
-            "url": article["url"],
-            "description": article["description"]
-        }
-        articles_list.append(short_article)
-    return HttpResponse(f'<p>currently showing{total_results}articles</p><p>{articles_list}</p>')
+    # response = requests.get(url)
+    # json_object = response.json()
+    # total_results = json_object["totalResults"]
+    # articles_list = []
+    # for article in json_object["articles"]:
+    #     short_article = {
+    #         "source": article["source"],
+    #         "author": article["author"],
+    #         "title": article["title"],
+    #         "url": article["url"],
+    #         "description": article["description"]
+    #     }
+    #     articles_list.append(short_article)
+    # return HttpResponse(f'<p>currently showing{total_results}articles</p><p>{articles_list}</p>')
+    return render(request, "home.html")
