0615867856ba3f15b526401a80d3fb3c5e146303
vinicius <vinimachado.s@gmail.com>
2020-09-13 10:07:29 +0200

    getting sources about crypto

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -7,17 +7,17 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = (f'http://newsapi.org/v2/sources?apiKey={api_key}')
+    url = (f'http://newsapi.org/v2/sources?q=crypto&sortBy=publishedAt&apiKey={api_key}')
     response = requests.get(url)
     json_object = response.json()
-    sources_list = json_object["sources"]
-    # for article in json_object["articles"]:
-    #     short_article = {
-    #         "source": article["source"],
-    #         "author": article["author"],
-    #         "title": article["title"],
-    #         "url": article["url"],
-    #         "description": article["description"]
-    #     }
-    #     articles_list.append(short_article)
-    return HttpResponse(f'<p>currently showing{len(sources_list)}articles</p><p>{sources_list}</p>')
+    articles_list = []
+    for article in json_object["articles"]:
+        short_article = {
+            "source": article["source"],
+            "author": article["author"],
+            "title": article["title"],
+            "url": article["url"],
+            "description": article["description"]
+        }
+        articles_list.append(short_article)
+    return HttpResponse(f'<p>currently showing{len(articles_list)}articles</p><p>{articles_list}</p>')
