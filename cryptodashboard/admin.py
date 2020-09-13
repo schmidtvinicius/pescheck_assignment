79e38fb64482dc9d85258f906371b3735f1a84b3
vinicius <vinimachado.s@gmail.com>
2020-09-13 12:42:36 +0200

    created the models

--- cryptodashboard/admin.py
+++ cryptodashboard/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import CryptoCurrency
+
 # Register your models here.
+@admin.register(CryptoCurrency)
+class CryptoCurrencyAdmin(admin.ModelAdmin):
+    list_display = ['name', 'code']
--- cryptodashboard/models.py
+++ cryptodashboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class CryptoCurrency(models.Model):
+    name = models.CharField(max_length=25)
+    code = models.CharField(max_length=3)
+
+    def __str__(self):
+        return f'{self.name} ({self.code})'
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=25)
+    description = models.TextField(blank=False)
+    url = models.TextField(blank=False)
+    currencies_discussed = models.ManyToManyField('CryptoCurrency', blank=False)
+
+
+
