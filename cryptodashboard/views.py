05752e40d7e87c03675bff0e0b53dca1fe3204b0
vinicius <vinimachado.s@gmail.com>
2020-09-13 10:27:14 +0200

    getting news from coin desk

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -7,7 +7,7 @@ api_key='3779ffddd95448f6ac0bc70bb87524e5'
 
 # Create your views here.
 def home(request):
-    url = (f'http://newsapi.org/v2/everything?q=bitcoin&apiKey={api_key}')
+    url = (f'http://newsapi.org/v2/top-headlines?q=bitcoin&sources=coin-desk&apiKey={api_key}')
     response = requests.get(url)
     json_object = response.json()
     articles_list = []
