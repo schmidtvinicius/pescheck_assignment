5067c13c6b76660d962f25f58ec60b8b082832d9
vinicius <vinimachado.s@gmail.com>
2020-09-13 12:45:10 +0200

    changed code length allowed

--- cryptodashboard/models.py
+++ cryptodashboard/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class CryptoCurrency(models.Model):
     name = models.CharField(max_length=25)
-    code = models.CharField(max_length=3)
+    code = models.CharField(max_length=10)
 
     def __str__(self):
         return f'{self.name} ({self.code})'
