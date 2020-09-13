2a7a9d1ec6cb7e173846c663633ab197955ede85
vinicius <vinimachado.s@gmail.com>
2020-09-13 15:33:24 +0200

    trying to populate database

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from newsapi import NewsApiClient
 import requests
+import json
 from .models import CryptoCurrency
 
 newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 
+CRYPTOS_AVAILABLE = 'cryptos_available.json'
+
 # Create your views here.
 def home(request):
+    raw_data = open(CRYPTOS_AVAILABLE, 'r')
+    crypto_json = json.loads(raw_data.read())
+    if not CryptoCurrency.objects.exists():
+        for crypto in crypto_json:
+            currency = CryptoCurrency()
+            currency.name = crypto["name"]
+            currency.code = crypto["code"]
+            currency.save()
     all_articles = newsapi.get_everything(q='bitcoin')
     return render(request, 'home.html', {
         'top_headlines': CryptoCurrency.objects.all(),
