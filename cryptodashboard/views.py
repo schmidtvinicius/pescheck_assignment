64b0f23eec1056f1cc21f780cbfa90bb4a64b5e4
vinicius <vinimachado.s@gmail.com>
2020-09-13 15:21:32 +0200

    showing all crypto currency

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from newsapi import NewsApiClient
 import requests
+from .models import CryptoCurrency
 
 newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 
@@ -9,5 +10,5 @@ newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 def home(request):
     all_articles = newsapi.get_everything(q='bitcoin')
     return render(request, 'home.html', {
-        'top_headlines': all_articles,
+        'top_headlines': CryptoCurrency.objects.all(),
     })
