2423ced8af9c5c17acd542f58ef277f849f86289
vinicius <vinimachado.s@gmail.com>
2020-09-14 20:09:03 +0200

    retrieving item from tuple

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -10,6 +10,8 @@ from .management.commands.load_articles import load_articles
 
 newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
 
+ORDER_BY_OPTIONS = ('Oldest', 'Newest', 'Alphabetical')
+
 # Create your views here.
 def home(request):
     #Article.objects.all().delete()
@@ -19,14 +21,17 @@ def home(request):
         'all_currencies': CryptoCurrency.objects.all(),
     })
 
-def currency_articles(request, currency_code='BTC'):
+def currency_articles(request, currency_code):
     try:
         currency = CryptoCurrency.objects.get(code=currency_code)
     except CryptoCurrency.DoesNotExist:
         raise Http404('Crypto currency not found!')
+    order_by = request.GET.get('order-by')
+    if order_by == None:
+        order_by = ORDER_BY_OPTIONS.index('Newest')
     if(not Article.objects.exists()):
         load_articles()
-    all_articles = Article.objects.all()
+    all_articles = Article.objects.all().order_by()
     matching_articles = []
     for article in all_articles:
         try:
@@ -35,5 +40,6 @@ def currency_articles(request, currency_code='BTC'):
         except CryptoCurrency.DoesNotExist:
             matching_currency = None
     return render(request, 'currency_articles.html', {
-        'articles': matching_articles, 
+        'articles': matching_articles,
+        'order_by_options': ORDER_BY_OPTIONS,
     })
