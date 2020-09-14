524286d8c86f25282cb0ee7865a12af2840b537f
vinicius <vinimachado.s@gmail.com>
2020-09-14 17:43:23 +0200

    chaged date format

--- cryptodashboard/management/commands/load_articles.py
+++ cryptodashboard/management/commands/load_articles.py
@@ -2,8 +2,12 @@ from newsapi import NewsApiClient
 from cryptodashboard.models import CryptoCurrency, Article
 from django.core.management import BaseCommand
 from .load_crypto_data import load_crypto_currencies
+from pytz import UTC
+from datetime import datetime
 
 newsapi = NewsApiClient(api_key='3779ffddd95448f6ac0bc70bb87524e5')
+
+DATETIME_FORMAT = '%Y/%m/%d'
 
 def load_articles():
     if not CryptoCurrency.objects.exists():
@@ -16,11 +20,12 @@ def load_articles():
         for tmp_article in all_articles:
             if not tmp_article["url"] in all_urls:
                 all_urls.append(tmp_article["url"])
+                published_at = UTC.localize(datetime.strptime(tmp_article["publishedAt"], DATETIME_FORMAT))
                 article = Article(title=tmp_article["title"], 
                                     author=tmp_article["author"], 
                                     description=tmp_article["description"], 
                                     url=tmp_article["url"], 
-                                    published_at=tmp_article["publishedAt"])
+                                    published_at=published_at)
                 article.save()
                 article.currencies_discussed.add(currency)
             else:
