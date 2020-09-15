79f995f64f4b712e9500b3a8b4bdcaa17f75e88e
vinicius <vinimachado.s@gmail.com>
2020-09-15 17:08:15 +0200

    test

--- cryptodashboard/views.py
+++ cryptodashboard/views.py
@@ -66,7 +66,7 @@ def currency_articles(request, currency_code):
             matching_currency = article.currencies_discussed.get(name=currency.name)
             matching_articles.append(article)
             split_url = article.url.split('/')
-            domains.append(split_url[1])
+            domains.append(split_url[2])
         except CryptoCurrency.DoesNotExist:
             matching_currency = None
     return render(request, 'currency_articles.html', {
